c83f73a38ecfa493cf269153193c7201093ae241
Adrià Cereto-Massagué <adrian.cereto@urv.cat>
2011-12-21 09:55:40 +0100

    Add PySide rthook

---
+++ support/rthooks/pyi_rth_pyside_qt4plugins.py
@@ -0,0 +1,23 @@
+# This hook is an adapted version of pyi_rth_qt4plugins.py for PySide
+import os
+import sys
+
+d = "qt4_plugins"
+d = os.path.join(sys._MEIPASS, d)
+
+
+# We remove QT_PLUGIN_PATH variable, beasuse we want Qt4 to load
+# plugins only from one path.
+if 'QT_PLUGIN_PATH' in os.environ:
+    #In some platform python is unable to unset environment variables,
+    #thus setting it to an empty string should do the trick there
+    os.environ['QT_PLUGIN_PATH'] = ''
+    del os.environ['QT_PLUGIN_PATH']
+
+
+# We cannot use QT_PLUGIN_PATH here, because it would not work when
+# PyQt4 is compiled with a different CRT from Python (eg: it happens
+# with Riverbank's GPL package).
+from PySide.QtCore import QCoreApplication
+# We set "qt4_plugins" as only one path for Qt4 plugins
+QCoreApplication.setLibraryPaths([os.path.abspath(d)])
